e9cdb5e8868fa5dc9bb8396f0d136a94c76ece17
YabTek <yabtekuam@gmail.com>
2023-03-24 10:22:57 +0300

    change index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const a = 1
 const b = 1
-console.log(a+b)
+console.log(a-b)
 //we need to ignore the file before commiting it 
 // if it is hashed once we need to unhash it inorder to ignore 
+//Two ways to create a branch 
+// git checkout -b name.newfile
+// git branch name.newfile
