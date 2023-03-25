05bcaca7f490f5eabb2634949a9314982347c266
YabTek <yabtekuam@gmail.com>
2023-03-25 13:52:12 +0300

    change files

--- operations.py
+++ operations.py
@@ -2,17 +2,23 @@
 class ArthimeticClass:
     def add(x, y):
         # adding two numbers
-        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+        if not isinstance(x, (int,float)) or not isinstance(y, (int,float)):
             raise TypeError("Both inputs should be numbers")
         return x + y
     def subtract(x, y):
         # subtracting two numbers
+        if not isinstance(x, (int,float)) or not isinstance(y, (int,float)):
+            raise TypeError("Both inputs should be numbers")
         return x - y
     def multiply(x, y):
         # multiplying two numbers
+        if not isinstance(x, (int,float)) or not isinstance(y, (int,float)):
+            raise TypeError("Both inputs should be numbers")
         return x * y
     def divide(x, y):
         # dividing two numbers
+        if not isinstance(x, (int,float)) or not isinstance(y, (int,float)):
+            raise TypeError("Both inputs should be numbers")
         return x / y
 
 
--- test_operations.py
+++ test_operations.py
@@ -15,6 +15,55 @@ class TestArithmeticClass:
             ArthimeticClass.add(1, "2")
         with pytest.raises(TypeError):
             ArthimeticClass.add("1", "2")
+       
+        assert expected_output == actual_output
+
+    def test_subtract(self ):
+        # arrange
+        x, y = 2.0, 1.0
+        expected_output = 1.0
+        # act
+        actual_output = ArthimeticClass.subtract(x, y)
+        # assert
+        with pytest.raises(TypeError):
+            ArthimeticClass.subtract("1", 2)
+        with pytest.raises(TypeError):
+            ArthimeticClass.subtract(1, "2")
+        with pytest.raises(TypeError):
+            ArthimeticClass.subtract("1", "2")
+        
+        assert expected_output == actual_output
+    def test_multiply(self ):
+        # arrange
+        x, y = 1.0, 2.0
+        expected_output = 2.0
+        # act
+        actual_output = ArthimeticClass.multiply(x, y)
+        # assert
+        with pytest.raises(TypeError):
+            ArthimeticClass.multiply("1", 2)
+        with pytest.raises(TypeError):
+            ArthimeticClass.multiply(1, "2")
+        with pytest.raises(TypeError):
+            ArthimeticClass.multiply("1", "2")
+        
+        
+        assert expected_output == actual_output
+    def test_divide(self ):
+        # arrange
+        x, y = 2.0, 1.0
+        expected_output = 2.0
+        # act
+        actual_output = ArthimeticClass.divide(x, y)
+        # assert
+        with pytest.raises(TypeError):
+            ArthimeticClass.divide("1", 2)
+        with pytest.raises(TypeError):
+            ArthimeticClass.divide(1, "2")
+        with pytest.raises(TypeError):
+            ArthimeticClass.add("1", "2")
+        with pytest.raises(ZeroDivisionError):
+            ArthimeticClass.divide(1,0)
         
         assert expected_output == actual_output
         
